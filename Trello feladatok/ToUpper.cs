3f2cb53e33117cb9ded95a07cdadb3aa1bf56fc7
UltraMKF12 <32771024+UltraMKF12@users.noreply.github.com>
2020-11-08 14:40:05 +0200

    Create ToUpper.cs

---
+++ Trello feladatok/ToUpper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trello_feladatok_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = Console.ReadLine();
+            string two = Console.ReadLine();
+
+            string three = one + two;
+            Console.WriteLine(three.ToUpper());
+        }
+    }
+}
