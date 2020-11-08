d9d57e0596c8d12537d65f2deaa0b3e197e29d0d
UltraMKF12 <32771024+UltraMKF12@users.noreply.github.com>
2020-11-08 14:54:54 +0200

    Create Interfaces.cs

---
+++ Trello feladatok/Interfaces.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trello_feladatok_2
+{
+    class Program
+    {
+        interface IVehicle
+        {
+            void Start();
+            void Stop();
+        }
+
+        class Car : IVehicle
+        {
+            public string Color { get; set; }
+            public string Brand { get; set; }
+            public string Status { get; set; }
+
+            public void Start()
+            {
+                Console.WriteLine("Starting the car.");
+            }
+
+            public void Stop()
+            {
+                Console.WriteLine("Stopping the car.");
+            }
+        }
+
+        class Plane : IVehicle
+        {
+            public string Color { get; set; }
+            public string Brand { get; set; }
+            public string Type { get; set; }
+            public string Capacity { get; set; }
+
+            public void Start()
+            {
+                Console.WriteLine("WE ARE GOING TO FLY YEAH");
+            }
+
+            public void Stop()
+            {
+                Console.WriteLine("DOWN DOWN DOWN TO EARTH");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IVehicle[] vehicles = new IVehicle[5];
+            vehicles[0] = new Car();
+            vehicles[1] = new Car();
+            vehicles[2] = new Car();
+            vehicles[3] = new Plane();
+            vehicles[4] = new Plane();
+
+            while(true)
+            {
+                Console.WriteLine("Type in the vehicle index 0-4 >>> ");
+                int index = Convert.ToInt32(Console.ReadLine());
+                if (index == -1) break;
+
+                Console.WriteLine("start or stop >>> ");
+                switch(Console.ReadLine())
+                {
+                    case "start":
+                        vehicles[index].Start();
+                        break;
+                    
+                    case "stop":
+                        vehicles[index].Stop();
+                        break;
+                }
+            }
+        }
+    }
+}
